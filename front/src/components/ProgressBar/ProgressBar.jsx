8e32cb120907f19fd98f104e2ce2e49f5fce27e8
AshkanArabim <ashkan.arabim@gmail.com>
2023-03-24 12:53:59 -0600

    fix dynamic progress bar

--- front/src/components/ProgressBar/ProgressBar.jsx
+++ front/src/components/ProgressBar/ProgressBar.jsx
@@ -1,11 +1,30 @@
+import "./ProgressBar.css";
+// we will only use the array of user answers from props
+
+// define progressBar function
 export default function ProgressBar(props) {
-  let progressBoxes = [];
-  
+  console.log(props);
 
   return (
     <div className="progressBar">
-      {/* <progress id = 'testProgress' value = {props.percentage} max='100'>{props.percentage}</progress> */}
-      {progressBoxes}
+      {
+        // more divs based on length of props.answers
+        props.answers.map(
+          // assign the class name based on whether question is right, wrong, or unanswered
+          (answer, idx) => (
+            <div
+              key={idx}
+              className={
+                answer
+                  ? "correct"
+                  : answer === false
+                  ? "incorrect"
+                  : "unanswered"
+              }
+            ></div>
+          )
+        )
+      }
     </div>
-  )
+  );
 }
